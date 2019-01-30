7b6ed06e0f1735a4a1db86cf357aeba909804e24
Michael Park <mpark@pentamint.com>
2019-01-30 18:05:55 +0900

    fixed last column margin issue

--- sidebar.php
+++ sidebar.php
@@ -11,9 +11,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-<div class="col">
+
 <div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
-</div><!-- .col>-->
-</div><!-- .container -->
